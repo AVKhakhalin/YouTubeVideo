b6386ba8e7273be38f1cb249046d71e886dc6ffe
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-09-10 17:14:27 +0300

    Создан и настроен youtubeplayer.

--- app/src/main/java/com/youtube/film/player/youtubevideo/MainActivity.kt
+++ app/src/main/java/com/youtube/film/player/youtubevideo/MainActivity.kt
@@ -2,10 +2,26 @@ package com.youtube.film.player.youtubevideo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 class MainActivity: AppCompatActivity() {
+    private var youTubePlayerView: YouTubePlayerView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Инициализация проигрывателя
+        youTubePlayerView = findViewById<YouTubePlayerView>(R.id.activity_main_youtubePlayerView)
+        youTubePlayerView?.let { youTubePlayerView ->
+            lifecycle.addObserver(youTubePlayerView)
+            youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+                override fun onReady(youTubePlayer: YouTubePlayer) {
+                    youTubePlayer.loadVideo(VIDEO_ID, START_SECOND)
+                }
+            })
+        }
     }
 }
